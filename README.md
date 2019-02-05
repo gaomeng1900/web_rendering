6e87d25309250a42a0cd9dea1136207369548fa2
Simin西萌 <gaomeng1900@gmail.com>
2019-02-05 22:21:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # web_rendering
-future of web 3D rendering
+
+An experimental design of web 3D renderer.
+
+设想一个SOA（面向服务）的Web渲染引擎。
---
+++ api/api.js
@@ -0,0 +1,19 @@
+// import * as THREE from 'THREE'
+
+// class Scene {
+//     constructor() {}
+// }
+
+// class Renderable {
+//     constructor() {}
+// }
+
+// class Renderer {
+//     constructor() {}
+// }
+
+// const scene = new Scene()
+
+// function preRender (scene: Scene) {
+    
+// }
